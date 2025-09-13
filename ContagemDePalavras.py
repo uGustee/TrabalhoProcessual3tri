ef3816727485e01d3ba9f72084c2ae6c93e82a87
gustavodap <72631936+uGustee@users.noreply.github.com>
2025-09-13 18:31:25 -0300

    Update ContagemDePalavras.py

--- ContagemDePalavras.py
+++ ContagemDePalavras.py
@@ -1,1 +1,14 @@
+texto = input("Digite um texto: ")
 
+palavras = texto.split()
+qtd = len(palavras)
+mais_longa = max(palavras, key=len)
+
+# contar a mais repetida
+from collections import Counter
+contagem = Counter(palavras)
+mais_repetida = contagem.most_common(1)[0][0]
+
+print("Quantidade de palavras:", qtd)
+print("Palavra mais longa:", mais_longa)
+print("Palavra que mais aparece:", mais_repetida)
