9dbc92ff2528a3f41431b8020136cd1777b2eee9
gustavodap <72631936+uGustee@users.noreply.github.com>
2025-09-13 18:52:28 -0300

    Update OrdenacaoPersonalizada.py

--- OrdenacaoPersonalizada.py
+++ OrdenacaoPersonalizada.py
@@ -1,4 +1,10 @@
+# Q2: O usuário digita 10 números, sendo que um deles deve ser o seu número da chamada. Armazene em uma lista e depois mostre:
+    # - A lista original;
+    # - A lista ordenada crescente;
+    # - A lista sem números repetidos.
+
 numeros = []
+
 print("Digite 10 números (um deles deve ser seu número da chamada):")
 for i in range(10):
     n = int(input(f"Número {i+1}: "))
@@ -6,11 +12,13 @@ for i in range(10):
 
 print("Lista original:", numeros)
 
-numeros_ordenados = sorted(numeros)
-print("Lista ordenada crescente:", numeros_ordenados)
+numerosOrdenados = sorted(numeros)
 
-sem_repetidos = []
+print("Lista ordenada crescente:", numerosOrdenados)
+
+semRepetidos = []
+
 for n in numeros:
-    if n not in sem_repetidos:
-        sem_repetidos.append(n)
-print("Lista sem números repetidos:", sem_repetidos)
+    if n not in semRepetidos:
+        semRepetidos.append(n)
+print("Lista sem números repetidos:", semRepetidos)
